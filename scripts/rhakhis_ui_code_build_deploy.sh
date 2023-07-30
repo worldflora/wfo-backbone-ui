33f95abbc2daacf351df40c9804651da4dab2716
Roger Hyam <rhyam@rbge.org.uk>
2023-07-30 07:55:04 +0100

    new build scripts

---
+++ scripts/rhakhis_ui_code_build_deploy.sh
@@ -0,0 +1,9 @@
+# This will use node to build and deploy the code to the ui director
+# This should be copied to /var/wfo-list directory so we have a single control point
+# for safety the paths are fully qualified - don't want this run from the wrong director!
+cd /var/wfo-list/rhakhis/ui-src/wfo
+npm run build
+rm -rf /var/wfo-list/rhakhis/ui/*  # remove all the existing live artifacts
+cp -r /var/wfo-list/rhakhis/ui-src/build/* /var/wfo-list/rhakhis/ui # just copy everything over.
+
+
